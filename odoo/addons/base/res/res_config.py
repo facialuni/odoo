2add83899304d422bf0d12ad05ea3edcca6fd868
Raphael Collet <rco@openerp.com>
2017-02-28 16:23:42 +0100

    fixup! [IMP] odoo: avoid using `reduce` or import it from `functools`

--- odoo/addons/base/res/res_config.py
+++ odoo/addons/base/res/res_config.py
@@ -457,6 +457,7 @@ class ResConfigSettings(models.TransientModel, ResConfigModuleInstallationMixin)
                     'other':   ['other_field', ...],
                 }
         """
+        Group = self.env['res.groups']
         IrModule = self.env['ir.module.module']
         ref = self.env.ref
 
@@ -467,7 +468,7 @@ class ResConfigSettings(models.TransientModel, ResConfigModuleInstallationMixin)
             elif name.startswith('group_') and field.type in ('boolean', 'selection') and \
                     hasattr(field, 'implied_group'):
                 field_group_xmlids = getattr(field, 'group', 'base.group_user').split(',')
-                field_groups = sum(map(ref, field_group_xmlids))
+                field_groups = Group.concat(*map(ref, field_group_xmlids))
                 groups.append((name, field_groups, ref(field.implied_group)))
             elif name.startswith('module_') and field.type in ('boolean', 'selection'):
                 module = IrModule.sudo().search([('name', '=', name[7:])], limit=1)
