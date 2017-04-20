2f619a7fa5ddeaf6e0bfb0fdad39f1ea036e6e69
Mitali Patel <mpa@odoo.com>
2017-04-20 11:15:17 +0530

    [REM] odoo: removed amount to text file from tools because its not working

--- odoo/tools/__init__.py
+++ odoo/tools/__init__.py
@@ -10,8 +10,6 @@ from .convert import *
 from .translate import *
 from .graph import graph
 from .image import *
-from .amount_to_text import *
-from .amount_to_text_en import *
 from .pdf_utils import *
 from .yaml_import import *
 from .sql import *
