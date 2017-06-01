3caab26450ebd3af38f8e61e3c11c98f84cae006
fka-odoo <fka@openerp.com>
2017-06-01 18:41:12 +0530

    [IMP] l10n_in: Set default sale & purchase tax in account configuration. Rename journal name 'Customer Invoices' to 'Tax Invoices'. Added 'Export Invoices' & 'Retail Invoices' journal.

--- addons/l10n_in/models/__init__.py
+++ addons/l10n_in/models/__init__.py
@@ -2,5 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import account_invoice
+from . import chart_template
 from . import product_template
 from . import res_company
+from . import res_config
---
+++ addons/l10n_in/models/res_config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+
+
+class AccountConfigSettings(models.TransientModel):
+    _inherit = 'account.config.settings'
+
+    @api.model
+    def get_default_tax_fields(self, fields):
+        res = super(AccountConfigSettings, self).get_default_tax_fields(fields=fields)
+        if self.env.user.company_id.chart_template_id == self.env.ref('l10n_in.indian_chart_template_standard'):
+            res['default_purchase_tax_id'] = self.env.ref('l10n_in.sgst_goods_5').id
+            res['default_sale_tax_id'] = self.env.ref('l10n_in.sgst_good_5').id
+        return res
