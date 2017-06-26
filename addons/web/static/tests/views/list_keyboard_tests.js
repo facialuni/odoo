89f993a0660553c6b4361fba9bafbb08f1f50759
Kishan Gajjar <kig@odoo.com>
2017-06-26 16:27:50 +0530

    [IMP] web: remove on blur event because of blur event sometimes test-case getting failed

--- addons/web/static/tests/views/list_keyboard_tests.js
+++ addons/web/static/tests/views/list_keyboard_tests.js
@@ -63,11 +63,6 @@ QUnit.module('Views', {
             return e;
         }
 
-        var lastActiveRow;
-        $('.o_data_row input').on('blur', function(e){
-            lastActiveRow = $(e.currentTarget).closest('.o_data_row');
-        });
-
         var $firstRecord = list.$el.find('.o_data_row').first();
         $firstRecord.find('input').trigger('click').focus();
 
@@ -75,29 +70,31 @@ QUnit.module('Views', {
 
         // Press down key will select next record
         $firstRecord.find('input').trigger(upDownKey(40));
+        var $lastActiveRow = $firstRecord.next();
         assert.ok(!$firstRecord.hasClass('o_row_selected'), 'First record unselected');
-        assert.ok($(lastActiveRow.next()).hasClass('o_row_selected'), 'Second record selected');
+        assert.ok($($firstRecord.next()).hasClass('o_row_selected'), 'Second record selected');
 
         // On shift + down select currrent and next record
         shiftKeyPress(40);
-        assert.ok($(lastActiveRow,lastActiveRow.next()).hasClass('o_row_selected'), "Select currrent and next record");
+        assert.ok($($lastActiveRow,$lastActiveRow.next()).hasClass('o_row_selected'), "Select currrent and next record");
 
         // On Ctrl + down will transfer focus to next record but don't select it
         controlKeyPress(40);
-        assert.ok($(lastActiveRow.next()).hasClass('o_row_focused'), "Next record is focused");
-
+        $lastActiveRow = $lastActiveRow.next();
+        assert.ok($($lastActiveRow.next()).hasClass('o_row_focused'), "Next record is focused");
         // On Ctrl + up will transfer focus to previous record but don't select it
         controlKeyPress(38);
-        assert.ok($(lastActiveRow.prev()).hasClass('o_row_focused'), "previous record is focused");
-
+        assert.ok($lastActiveRow.hasClass('o_row_focused'), "previous record is focused");
         // On shift + up unselect previous record
+        $lastActiveRow = $lastActiveRow.prev();
         shiftKeyPress(38);
-        assert.ok(!$(lastActiveRow.prev()).hasClass('o_row_selected'), "Unselect previous record");
+        assert.ok(!$($lastActiveRow).hasClass('o_row_selected'), "Unselect previous record");
+        $lastActiveRow = $lastActiveRow.prev();
 
         // On up(arrow) key it will select previous record
         var $activeElement = getActiveRow();
         $activeElement.find('input').trigger(upDownKey(38));
-        assert.ok($(lastActiveRow.prev()).hasClass('o_row_selected'), "Select previous record");
+        assert.ok($($lastActiveRow).hasClass('o_row_selected'), "Select previous record");
 
         list.destroy();
     });
