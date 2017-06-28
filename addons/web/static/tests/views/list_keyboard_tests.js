1ede1a5d57ea77160477db31c5e7bd9cbd7a5e93
Kishan Gajjar <kig@odoo.com>
2017-06-28 10:52:17 +0530

    [IMP] web : Code Improvement remove upDownKey and getActiveRow function.

--- addons/web/static/tests/views/list_keyboard_tests.js
+++ addons/web/static/tests/views/list_keyboard_tests.js
@@ -49,19 +49,8 @@ QUnit.module('Views', {
             var e = $.Event("keydown");
             e.which = direction;
             e.ctrlKey = true;
-            var $activeElement = getActiveRow();
-            $activeElement.find('input').trigger(e);
+            $(document.activeElement).trigger(e);
         };
-
-        var getActiveRow = function () {
-            return $(document.activeElement).closest('.o_data_row');
-        };
-
-        var upDownKey = function (direction) {
-            var e = $.Event("keydown");
-            e.which = direction;
-            return e;
-        }
 
         var $firstRecord = list.$el.find('.o_data_row').first();
         $firstRecord.find('input').trigger('click').focus();
@@ -69,22 +58,25 @@ QUnit.module('Views', {
         assert.ok($firstRecord.hasClass('o_row_selected'),'First record selected');
 
         // Press down key will select next record
-        $firstRecord.find('input').trigger(upDownKey(40));
-        var $lastActiveRow = $firstRecord.next();
+        $firstRecord.find('input').trigger($.Event('keydown', {which: $.ui.keyCode.DOWN}));
+        var $lastActiveRow = $firstRecord;
         assert.ok(!$firstRecord.hasClass('o_row_selected'), 'First record unselected');
-        assert.ok($($firstRecord.next()).hasClass('o_row_selected'), 'Second record selected');
+        assert.ok($($lastActiveRow.next()).hasClass('o_row_selected'), 'Second record selected');
 
         // On shift + down select currrent and next record
         shiftKeyPress(40);
+        $lastActiveRow = $lastActiveRow.next();
         assert.ok($($lastActiveRow,$lastActiveRow.next()).hasClass('o_row_selected'), "Select currrent and next record");
 
         // On Ctrl + down will transfer focus to next record but don't select it
         controlKeyPress(40);
         $lastActiveRow = $lastActiveRow.next();
         assert.ok($($lastActiveRow.next()).hasClass('o_row_focused'), "Next record is focused");
+
         // On Ctrl + up will transfer focus to previous record but don't select it
         controlKeyPress(38);
-        assert.ok($lastActiveRow.hasClass('o_row_focused'), "previous record is focused");
+        assert.ok($lastActiveRow.hasClass('o_row_focused'), "Previous record is focused");
+
         // On shift + up unselect previous record
         $lastActiveRow = $lastActiveRow.prev();
         shiftKeyPress(38);
@@ -92,8 +84,7 @@ QUnit.module('Views', {
         $lastActiveRow = $lastActiveRow.prev();
 
         // On up(arrow) key it will select previous record
-        var $activeElement = getActiveRow();
-        $activeElement.find('input').trigger(upDownKey(38));
+        $(document.activeElement).trigger($.Event('keydown', {which: $.ui.keyCode.UP}));
         assert.ok($($lastActiveRow).hasClass('o_row_selected'), "Select previous record");
 
         list.destroy();
