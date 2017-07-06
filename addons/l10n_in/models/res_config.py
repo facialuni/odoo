2bcf820f935e91948cf72ac84af106bb32c0cde5
fka-odoo <fka@openerp.com>
2017-07-06 18:55:18 +0530

    [FIX] l10n_in: deprecated get_default_tax_fields method.

--- addons/l10n_in/models/res_config.py
+++ addons/l10n_in/models/res_config.py
@@ -8,8 +8,8 @@ class AccountConfigSettings(models.TransientModel):
     _inherit = 'account.config.settings'
 
     @api.model
-    def get_default_tax_fields(self, fields):
-        res = super(AccountConfigSettings, self).get_default_tax_fields(fields=fields)
+    def get_values(self):
+        res = super(AccountConfigSettings, self).get_values()
         if self.env.user.company_id.chart_template_id == self.env.ref('l10n_in.indian_chart_template_standard'):
             res['default_purchase_tax_id'] = self.env.ref('l10n_in.sgst_purchase_5').id
             res['default_sale_tax_id'] = self.env.ref('l10n_in.sgst_sale_5').id
