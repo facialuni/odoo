cfaf2066acc45d22e2b4095880bcab9c74253ecf
Deep Patel <dep@odoo.com>
2017-07-11 14:24:49 +0530

    [IMP] base : add base template for settings, change settings for project modules

--- addons/pad_project/models/project_config_settings.py
+++ addons/pad_project/models/project_config_settings.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 
 class ProjectConfigSettings(models.TransientModel):
-    _inherit = "project.config.settings"
+    _inherit = "res.config.settings"
 
     pad_server = fields.Char(related="company_id.pad_server", string="Pad Server")
     pad_key = fields.Char(related="company_id.pad_key", string="Pad Api Key")
