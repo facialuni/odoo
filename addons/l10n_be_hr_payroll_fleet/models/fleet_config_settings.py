919d1513a27e91d58b8da39dc1555752f8fa0990
Deep Patel <dep@odoo.com>
2017-07-17 11:54:25 +0530

    [IMP] hr_payroll_fleet  : change setting

--- addons/l10n_be_hr_payroll_fleet/models/fleet_config_settings.py
+++ addons/l10n_be_hr_payroll_fleet/models/fleet_config_settings.py
@@ -5,7 +5,6 @@ from odoo import api, fields, models
 
 
 class FleetConfigSettings(models.TransientModel):
-    _name = 'fleet.config.settings'
     _inherit = ['res.config.settings']
 
     max_unused_cars = fields.Integer(string='Maximum unused cars')
