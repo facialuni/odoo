487d603c63570b3c3669b10cbacdc30587e0c500
amoyaux <arm@odoo.com>
2017-07-18 10:21:44 +0200

    [FIX] stock : initial demand should be 0 on create
    When creating a new line in a picking, the initial demand by default should be 0
    
    This commit change the default value for product_uom_qty to 0.0

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -52,7 +52,7 @@ class StockMove(models.Model):
     product_uom_qty = fields.Float(
         'Initial Demand',
         digits=dp.get_precision('Product Unit of Measure'),
-        default=1.0, required=True, states={'done': [('readonly', True)]},
+        default=0.0, required=True, states={'done': [('readonly', True)]},
         help="This is the quantity of products from an inventory "
              "point of view. For moves in the state 'done', this is the "
              "quantity of products that were actually moved. For other "
