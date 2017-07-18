b7e37bbd8ebb24606b3422f5317a411613a365c5
Hetashree Chauhan <hch@odoo.com>
2017-07-18 11:12:23 +0530

    [IMP] hr_expense: expense registeration failed message has been sent successfully

--- addons/hr_expense/models/mail_thread.py
+++ addons/hr_expense/models/mail_thread.py
@@ -11,17 +11,18 @@ class MailThread(models.AbstractModel):
     @api.model
     def message_new(self, msg_dict, custom_values=None):
         model = self._context.get('thread_model') or self._name
-        res = super(MailThread, self).message_new(msg_dict, custom_values)
         if model == 'hr.expense':
-            data = {}
             RecordModel = self.env[model]
-            if isinstance(custom_values, dict):
-                data = custom_values.copy()
-            data['product_id'] = self.env['product.product'].search(['|', ('name', '=ilike', data['name']), ('default_code', '=ilike', data['name'])]).id or data['product_id']
-            res.write({'product_id': data['product_id']})
-            if res:
+            custom_values['product_id'] = self.env['product.product'].search(['|', ('name', '=ilike', custom_values['name']), ('default_code', '=ilike', custom_values['name'])]).id or custom_values['product_id']
+            if custom_values.get('employee_id'):
+                res = super(MailThread, self).message_new(msg_dict, custom_values)
                 template_id = self.env.ref('hr_expense.email_template_hr_expense_success')
+                template_id.send_mail(res.employee_id.user_id.partner_id.id)
+                return res
             else:
+                base_partner = self.env.ref('base.partner_root')
                 template_id = self.env.ref('hr_expense.email_template_hr_expense_falied')
-            template_id.send_mail(res.employee_id.user_id.partner_id.id)
-        return res
+                template_id.write({'email_to': msg_dict['from'].split('<')[1].split('>')[0]})
+                template_id.sudo().send_mail(base_partner.id, force_send=True)
+            return False
+        return super(MailThread, self).message_new(msg_dict, custom_values)
