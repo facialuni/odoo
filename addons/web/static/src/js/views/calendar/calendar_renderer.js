2f7fb40be5965c2416e600006b1d861e997dd85f
Bhavesh Patel <bht@odoo.com>
2017-07-18 15:37:20 +0530

    [IMP] web: improve 'calendar_renderer.js' for language direction RTL to LTR and LTR to RTL using mixin

--- addons/web/static/src/js/views/calendar/calendar_renderer.js
+++ addons/web/static/src/js/views/calendar/calendar_renderer.js
@@ -332,6 +332,7 @@ return AbstractRenderer.extend({
             },
             height: 'parent',
             unselectAuto: false,
+            isRTL: _t.database.parameters.direction == 'rtl' ? true : false,
         });
 
         this.$calendar.fullCalendar(fc_options);
