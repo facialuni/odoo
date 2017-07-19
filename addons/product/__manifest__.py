8d65da31ac1bb844f4baa828cf2f50bf4b600772
Prakash Prajapati <ppr@odoo.com>
2017-07-19 19:54:51 +0530

    [REM] auth_signup: remove tooltip [IMP] base: Reporting: rename menu "Reporting" to "Reports"

--- addons/product/__manifest__.py
+++ addons/product/__manifest__.py
@@ -31,7 +31,6 @@ Print product labels with barcode.
         'security/product_security.xml',
         'security/ir.model.access.csv',
         'wizard/product_price_list_views.xml',
-        'views/base_config_settings_views.xml',
         'views/product_attribute_views.xml',
         'views/product_uom_views.xml',
         'views/product_views.xml',
