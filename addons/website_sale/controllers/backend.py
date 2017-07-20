303e9f4c4f06d4206ce79c20c6bf94e949c68b65
Christophe Monniez <moc@odoo.com>
2017-07-20 14:54:54 +0200

    [FIX] website_sale: Default filter email to send
    The list view of abandoned carts should be filtered by default to show
    only ones where the recovery email was not sent.
    
    The dashboard count is adapated accordingly.

--- addons/website_sale/controllers/backend.py
+++ addons/website_sale/controllers/backend.py
@@ -90,6 +90,7 @@ class WebsiteSaleBackend(WebsiteBackend):
                 ('order_line', '!=', 'False'),
                 ('partner_id', '!=', request.env.ref('base.public_partner').id),
                 ('date_order', '<=', fields.Datetime.to_string(datetime.utcnow() - timedelta(hours=1))),
+                ('cart_recovery_email_sent', '=', 'False')
             ]),
             payment_to_capture_count=request.env['payment.transaction'].search_count([
                 ('state', '=', 'authorized'),
