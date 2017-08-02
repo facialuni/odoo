ae75fad184bfbea97f70328e206f0dd25a9a52e0
Ankit Joshi <ajo@odoo.com>
2017-08-02 19:22:56 +0530

    [MIG]stock: test_wiseoperator yml test case convert to python test case.

--- addons/stock/tests/test_wiseoperator.py
+++ addons/stock/tests/test_wiseoperator.py
@@ -98,47 +98,43 @@ class TestWiseOperator(TestStockCommon):
                 'product_uom': self.uom_unit_id
             })]
         })
-
         # Assign and confirm
         self.delivery_order_wise2.action_confirm()
         self.delivery_order_wise2.action_assign()
         self.assertEqual(self.delivery_order_wise2.state, 'assigned', 'wrong state in delivery order.')
 
         # The operator is a wise guy and decides to do the opposite of what Odoo proposes.  He uses the products reserved on picking 1 on picking 2 and vice versa
-
-        loc_14 = self.ref('stock.stock_location_14')
-        pack_ids1_loc = self.delivery_order_wise1.move_line_ids.location_id.id
-        pack_ids2 = self.delivery_order_wise2.move_line_ids
-        self.assertEqual(pack_ids1_loc, loc_14, 'wrong stock location.')
-        self.assertEqual(set(pack_ids2.mapped('location_id.id')), set([self.ref('stock.stock_location_components'), loc_14]), 'worng location.')
-
-        self.delivery_order_wise1.move_line_ids.write({'picking_id': self.delivery_order_wise2.id})
-        self.delivery_order_wise2.move_line_ids.write({'picking_id': self.delivery_order_wise1.id})
-
-        # put the move lines from picking2 into picking1
         picking1 = self.delivery_order_wise1
         picking2 = self.delivery_order_wise2
-        pack_ids1 = self.delivery_order_wise1.move_line_ids
-        move1 = self.delivery_order_wise1.move_lines[0]
-        move2 = self.delivery_order_wise2.move_lines[0]
+        move1 = picking1.move_lines
+        move2 = picking2.move_lines
+        pack_ids1 = picking1.move_line_ids
+        pack_ids2 = picking2.move_line_ids
+
+        location_id14 = self.ref('stock.stock_location_14')
+        pack_ids1_loc = self.delivery_order_wise1.move_line_ids.location_id.id
+        self.assertEqual(pack_ids1_loc, location_id14, 'wrong stock location.')
+        self.assertEqual(set(pack_ids2.mapped('location_id.id')), set([self.ref('stock.stock_location_components'), location_id14]), 'Wrong location detected')
+
+        # put the move lines from picking2 into picking1
         for pack_id2 in pack_ids2:
             new_pack_id1 = pack_id2.copy(default={'picking_id': picking1.id, 'move_id': move1.id})
             new_pack_id1.qty_done = new_pack_id1.product_qty
             new_pack_id1.with_context(bypass_reservation_update=True).product_uom_qty = 0
-
         new_move_lines = picking1.move_line_ids.filtered(lambda p: p.qty_done)
-        qty_sum = sum(new_move_lines.mapped('product_qty'))
-        qty_done_sum = sum(new_move_lines.mapped('qty_done'))
-        self.assertEqual(qty_sum, 0.0, 'wrong product quantity.')
-        self.assertEqual(qty_done_sum, 5.0, 'wrong product done quantity.')
-        self.assertEqual(set(new_move_lines.mapped('location_id.id')), set([self.ref('stock.stock_location_components'), loc_14]), 'wrong location')
+        self.assertEqual(sum(new_move_lines.mapped('product_qty')), 0, 'wrong product quantity.')
+        self.assertEqual(sum(new_move_lines.mapped('qty_done')), 5, 'wrong product done quantity.')
+        self.assertEqual(set(new_move_lines.mapped('location_id.id')), set([self.ref('stock.stock_location_components'), location_id14]), 'Wrong location detected')
 
         # put the move line from picking1 into picking2
-        new_pack_id2 = pack_ids1[0].copy(default={'picking_id': self.delivery_order_wise1.id, 'move_id': move2.id})
+        new_pack_id2 = pack_ids1.copy(default={'picking_id': picking2.id, 'move_id': move2.id})
         new_pack_id2.qty_done = new_pack_id2.product_qty
         new_pack_id2.with_context(bypass_reservation_update=True).product_uom_qty = 0
-
         new_move_lines = picking2.move_line_ids.filtered(lambda p: p.qty_done)
+        self.assertEqual(len(new_move_lines), 1, 'wrong move lines.')
+        self.assertEqual(sum(new_move_lines.mapped('product_qty')), 0, 'wrong product quantity.')
+        self.assertEqual(sum(new_move_lines.mapped('qty_done')), 5, 'wrong product done quantity.')
+        self.assertEqual(new_move_lines.location_id.id, location_id14, 'Wrong location detected')
 
         # Process this picking
         self.delivery_order_wise1.do_transfer()
@@ -146,3 +142,13 @@ class TestWiseOperator(TestStockCommon):
         # Check there was no negative quant created by this picking
         self.records = self.StockQuant.search([('product_id', '=', self.product_wise.id), ('quantity', '<', 0.0)])
         self.assertEqual(len(self.records.ids), 0, 'This should not have created a negative quant')
+
+        # Check the other delivery order has changed its state back to partially available
+        self.assertEqual(self.delivery_order_wise2.state, 'partially_available', "Delivery order 2 should be back in confirmed state")
+
+        # Process the second picking
+        self.delivery_order_wise2.do_transfer()
+
+        # Check all quants are in Customers and there are no negative quants anymore
+        self.records = self.StockQuant.search([('product_id', '=', self.product_wise.id)])
+        self.assertTrue(all([x.location_id.id == self.customer_location_id and x.qty > 0.0 for x in self.records]), 'Negative quant or wrong location detected')
