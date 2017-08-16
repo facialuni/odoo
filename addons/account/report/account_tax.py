0deab2476333bcab08f7c480b214d1fd7f83a210
Prakash Prajapati <ppr@odoo.com>
2017-08-16 17:16:58 +0530

    [Add] account : Add PDF Tax Report on community

--- addons/account/report/account_tax.py
+++ addons/account/report/account_tax.py
@@ -15,6 +15,13 @@ class ReportTax(models.AbstractModel):
         purchase_tax_line= self.env['account.tax'].search([('type_tax_use', '=', 'purchase')])
         self.model = self.env.context.get('active_model')
         docs = self.env[self.model].browse(self.env.context.get('active_ids', []))
+        all_sales = self.env['account.invoice'].search([('type', '=', 'out_invoice')])
+        all_sale_price = sum(all_sales.mapped('amount_untaxed'))
+        all_sale_tax = sum(all_sales.mapped('amount_tax'))
+        all_purchase = self.env['account.invoice'].search([('type', '=', 'in_invoice')])
+        all_purchase_price = sum(all_purchase.mapped('amount_untaxed'))
+        all_purchase_tax = sum(all_purchase.mapped('amount_tax'))
+
         return {
             'doc_ids': self.ids,
             'doc_model': self.model,
@@ -24,4 +31,8 @@ class ReportTax(models.AbstractModel):
             'sale_tax': sale_tax_line.name,
             'purchase_line': purchase_tax_line.type_tax_use,
             'purchase_tax': purchase_tax_line.name,
+            'all_sale_price': all_sale_price,
+            'all_sale_tax': all_sale_tax,
+            'all_purchase_price': all_purchase_price,
+            'all_purchase_tax':all_purchase_tax,
         }
--- addons/account/wizard/account_report_tax.py
+++ addons/account/wizard/account_report_tax.py
@@ -8,9 +8,10 @@ class AccountTaxReport(models.TransientModel):
     _name = 'account.tax.report'
     _description = 'Tax Report'
 
-    target_move = fields.Selection([('cash_basis', 'Cash Basis Method')], string='Options')
+    options = fields.Selection([('cash_basis', 'Cash Basis Method')], string='Options')
     
     def _print_report(self, data):
         data = self.pre_print_report(data)
+        data['form'].update(self.read(['options'])[0])
         records = self.env[data['model']].browse(data.get('ids', []))
         return self.env.ref('account.action_report_account_tax').report_action(records, data=data)
