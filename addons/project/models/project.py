c4392755f85e2a108d6b7c6cea76fd38fc349232
Jérome Maes <jem@openerp.com>
2017-08-17 09:38:16 +0200

    [REM] website_rating_project: move feature in project
    Displaying the rating of project is moved directly
    in project module, since it does not depends on website.
    User may want to display rating of its support project
    even without website.

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -246,6 +246,7 @@ class Project(models.Model):
         ('daily', 'Daily'), ('weekly', 'Weekly'), ('bimonthly', 'Twice a Month'),
         ('monthly', 'Once a Month'), ('quarterly', 'Quarterly'), ('yearly', 'Yearly')
     ], 'Rating Frequency')
+    portal_show_rating = fields.Boolean('Rating visible in Website', copy=False, oldname='website_published')
 
     _sql_constraints = [
         ('project_date_greater', 'check(date >= date_start)', 'Error! project start-date must be lower than project end-date.')
@@ -347,6 +348,13 @@ class Project(models.Model):
 
     def action_view_all_rating(self):
         """ return the action to see all the rating of the project, and activate default filters """
+        if self.portal_show_rating:
+            return {
+                'type': 'ir.actions.act_url',
+                'name': "Redirect to the Website Projcet Rating Page",
+                'target': 'self',
+                'url': "/project/rating/%s" % (self.id,)
+            }
         action = self.env['ir.actions.act_window'].for_xml_id('project', 'rating_rating_action_view_project_rating')
         action['name'] = _('Ratings of %s') % (self.name,)
         action_context = safe_eval(action['context']) if action['context'] else {}
